e9aee2aadb245d2f53c2f6e31fc978e125b94a8e
dongtao <dongtao@111.com.cn>
2017-05-16 23:59:09 +0800

    reset migration

---
+++ database/migrations/2017_05_15_231015_create_wallet_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWalletTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallet', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->decimal('money',7,2);
+            $table->unsignedInteger('user_id');
+            $table->string('valiable')->default('true');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('wallet');
+    }
+}
